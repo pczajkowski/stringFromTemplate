fdd2b783ac652d7af98ebe3d4655161be181f6c6
Piotr Czajkowski <pczajkowski@gmail.com>
2020-01-22 16:49:02 +0100

    First version

---
+++ stringFromTemplate.go
@@ -0,0 +1,22 @@
+package stringFromTemplate
+
+import (
+	"strings"
+	"text/template"
+)
+
+func ToString(templateString string, object interface{}) string {
+	var buffer strings.Builder
+
+	tmpl, err := template.New("default").Parse(templateString)
+	if err != nil {
+		return err.Error()
+	}
+
+	err = tmpl.Execute(&buffer, object)
+	if err != nil {
+		return err.Error()
+	}
+
+	return buffer.String()
+}
---
+++ stringFromTemplate_test.go
@@ -0,0 +1,63 @@
+package stringFromTemplate
+
+import (
+	"testing"
+)
+
+type Something struct {
+	First  int
+	Second string
+}
+
+const expected = `Something here 15
+And something else here and the string`
+
+func TestToString(t *testing.T) {
+	const templateString = `Something here {{.First}}
+And something else here {{.Second}}`
+
+	test := Something{First: 15, Second: "and the string"}
+
+	result := ToString(templateString, test)
+
+	if expected != result {
+		t.Errorf("%v", result)
+	} else {
+		t.Log("String constructed properly.")
+	}
+}
+
+func TestToStringBadTemplate(t *testing.T) {
+	const templateString = `Something here {{.First()}}
+And something else here {{.Second}}`
+
+	test := Something{First: 15, Second: "and the string"}
+
+	result := ToString(templateString, test)
+
+	if expected == result {
+		t.Error("It should fail on template parsing!")
+	} else {
+		t.Log(result)
+	}
+}
+
+func TestToStringBadObject(t *testing.T) {
+	const templateString = `Something here {{.First}}
+And something else here {{.Second}}`
+
+	type Something struct {
+		Third  int
+		Second string
+	}
+
+	test := Something{Third: 15, Second: "and the string"}
+
+	result := ToString(templateString, test)
+
+	if expected == result {
+		t.Error("It should fail!")
+	} else {
+		t.Log(result)
+	}
+}
